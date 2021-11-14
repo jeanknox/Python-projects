e28e645323a87e450912e8703859351197d28fa7
JeanMarcio <Jeankx@live.com>
2021-11-14 19:59:12 -0300

    Barcode reader is now working flawlwss

--- Barcode reader/code_reader.py
+++ Barcode reader/code_reader.py
@@ -1,17 +1,23 @@
-from qrtools.qrtools import QR
+from pyzbar.pyzbar import decode
+from cv2 import imread
 import os
-def get_files():
-    files = [arquivo for arquivo in os.listdir() if arquivo.endswith(('.jpg', '.png'))]
+
+
+def get_files(file_etxtension=".jpg,.png"):
+    files = [arquivo for arquivo in os.listdir()
+             if arquivo.endswith((tuple(file_etxtension.split(","))))]
     return files
+
 
 def get_codebar(files):
     for arquivo in files:
-        my_code = QR(filename=arquivo)
-        if my_code.decode():
+        try:
+            code_bar = decode(imread(arquivo))
             with open('retorno.txt', 'w') as retorno:
-                retorno.writelines(f'{arquivo}:{my_code.data_to_streing()}')
+                retorno.writelines(f'{arquivo}: {code_bar[0][0]}')
+        except:
+            print("Erro ao ler a imagem:{arquivo}")
 
 
-if __name__ =='__main__':
-    files = get_files()
-    get_codebar(files)
+if __name__ == '__main__':
+    get_codebar(get_files())
